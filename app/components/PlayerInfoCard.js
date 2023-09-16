d5abc784b2b51d767ddb1c828ccc0f90bd3255eb
CharlieNData <charlesejewers@gmail.com>
2023-09-16 13:40:52 +0100

    added player card data

--- app/components/PlayerInfoCard.js
+++ app/components/PlayerInfoCard.js
@@ -1,15 +1,67 @@
 import React from 'react';
+import { Value } from 'sass';
 
 export default function PlayerInfoCard(props) {
 
   const player = props.playerSelected;
   const team = props.teamPlaysFor;
 
+  const positions = [
+    'Goalkeeper', 
+    'Defender', 
+    'Midfielder', 
+    'Forward'
+  ];
+
+  const stats = [
+    {
+      title: 'Goals',
+      value:'goals_scored'
+    },
+    {
+      title: 'Assists',
+      value:'assists'
+    },
+    {
+      title: 'Form',
+      value:'form'
+    },
+    {
+      title: 'Expected Goals',
+      value:'expected_goals'
+    },
+    {
+      title: 'Expected Assists',
+      value: 'expected_assists'
+    },
+    {
+      title: 'Ownership',
+      value:'selected_by_percent'
+    }
+  ];
+
+  const buildStat = (title, value) => {
+    return (
+      <tr className='fpl-player-info-card__stats__row'>
+        <td className='fpl-player-info-card__stats__item'>{title}: </td>
+        <td className='fpl-player-info-card__stats__item'>{player[value]}</td>
+      </tr>
+    );
+  }
+  
+
   return (
     <div className='fpl-player-info-card'>
       <div className='fpl-player-info-card__titles'>
-        <h2>{player.first_name + ' ' + player.second_name}</h2>
-        <p>{team.name}</p>
+        <h1>{player.first_name + ' ' + player.second_name}</h1>
+        <p>{positions[player.element_type-1]} | {team.name}</p>
+      </div>
+      <div className='fpl-player-info-card__stats--container'>
+        <table className='fpl-player-info-card__stats'>
+          {stats.map(function(s) {
+            return buildStat(s.title, s.value);
+          })}
+        </table>
       </div>
     </div>
   );
